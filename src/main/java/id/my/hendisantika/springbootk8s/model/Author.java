9d77de588a57f6adf0e326964aeaddf6d1a7679f
hendisantika <hendisantika@yahoo.co.id>
2026-01-17 12:55:04 +0700

    feat: Add Author Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootk8s/model/Author.java
@@ -0,0 +1,38 @@
+package id.my.hendisantika.springbootk8s.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.awt.print.Book;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-k8s
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 17/01/26
+ * Time: 12.54
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@Table(name = "author")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "author")
+    private List<Book> books;
+}
