9fe8a62124352ec7d54bffc82f6c2407d7efaf8e
hendisantika <hendisantika@yahoo.co.id>
2026-01-17 12:48:30 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootk8s/SpringbootK8sApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootK8sApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringbootK8sApplication.class, args);
+    }
+
+}
