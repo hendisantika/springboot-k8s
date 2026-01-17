bbb270620d70ddb07414728cbfe9d42f64bc9dcd
hendisantika <hendisantika@yahoo.co.id>
2026-01-17 12:58:45 +0700

    feat: Add AuthorRepository🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootk8s/repository/AuthorRepository.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.springbootk8s.repository;
+
+import id.my.hendisantika.springbootk8s.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-k8s
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 17/01/26
+ * Time: 12.58
+ * To change this template use File | Settings | File Templates.
+ */
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
