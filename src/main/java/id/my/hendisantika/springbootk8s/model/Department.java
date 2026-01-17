57909839dfe796e4d06a5bfeb9dc760c7372cbee
hendisantika <hendisantika@yahoo.co.id>
2026-01-17 12:56:12 +0700

    feat: Add Department Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootk8s/model/Department.java
@@ -0,0 +1,38 @@
+package id.my.hendisantika.springbootk8s.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-k8s
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 17/01/26
+ * Time: 12.55
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "department")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String deptName;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Employee> employees;
+
+}
