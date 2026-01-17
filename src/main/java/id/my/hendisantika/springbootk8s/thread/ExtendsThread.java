a2ee97d0855f4141b4833efee10f6cdb35416173
hendisantika <hendisantika@yahoo.co.id>
2026-01-17 13:08:47 +0700

    feat: Add ExtendsThread🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootk8s/thread/ExtendsThread.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.springbootk8s.thread;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-k8s
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 17/01/26
+ * Time: 13.08
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExtendsThread extends Thread {
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i <= 5; i++) {
+            System.out.print(Thread.currentThread().getName() + "Count===>" + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+}
