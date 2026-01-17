0331df6b5d92248cf803ce453b54a2c613778d64
hendisantika <hendisantika@yahoo.co.id>
2026-01-17 12:52:39 +0700

    feat: Add Cache🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootk8s/component/Cache.java
@@ -0,0 +1,33 @@
+package id.my.hendisantika.springbootk8s.component;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-k8s
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 17/01/26
+ * Time: 12.52
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class Cache implements CommandLineRunner {
+
+    @Autowired
+    private final UserService userService;
+
+    public Cache(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(userService.getUserById(Long.valueOf("1")));
+        System.out.println(userService.getUserById(Long.valueOf("1")));
+    }
+}
